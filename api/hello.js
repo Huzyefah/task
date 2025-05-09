8d923dab1ac2d99e37ba9c2a063c4869f6086287
Huzyefah <huzyefahsaqib13@gmail.com>
2025-05-09 12:00:29 +0500

    .

--- api/hello.js
+++ api/hello.js
@@ -1,12 +1,9 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 app.get("/hello", (req, res) => {
-  res.send("Hello from Render-deployed Express server!");
+  res.send("Hello from Vercel-deployed Express server!");
 });
-
-// Export app as a Vercel handler
 module.exports = (req, res) => {
   app(req, res);
 };
