ee4f00c81ec226edd30d61492594783b74db9733
Huzyefah <huzyefahsaqib13@gmail.com>
2025-05-09 12:30:23 +0500

    .

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/hello", (req, res) => {
-  res.send("Hello from root-level index.js on Vercel!");
+  res.send("Hello");
 });
 
 module.exports = app;
